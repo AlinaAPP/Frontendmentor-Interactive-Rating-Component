7d70696de0286aea2fc7cecf56793a280f2d6109
Alina Apostu <alinagabriela.greu@gmail.com>
2022-11-04 20:34:09 +0100

    js and bem changed

--- java/java.js
+++ java/java.js
@@ -4,26 +4,31 @@ const card = document.querySelector('#card');
 const rating = document.querySelector('#rating');
 
 let show_rating = document.querySelector('#show_rating');
-let buttons = document.querySelectorAll('#btn_rating button');
 
-const btn_rating = document.querySelector('#btn_rating');
+let allBtn = document.querySelectorAll('.btn_rating');
+/* console.log(allBtn); */
 
-//add a click event listener to all the buttons inside a div
-btn_rating.addEventListener('click', (e) => {
-// Check to see if it was a button that was clicked
-    if(e.target.classList.contains('btn_rating')) {
+allBtn.forEach((btn, i) => {
+    btn.addEventListener('click', function() {
+        /* console.log(btn);
+        console.log(i); */
         
-        // Loop over all the buttons & remove the active class
-        buttons.forEach(function(button){
-            button.classList.remove('active');
-        });
+        let btn_index = i+1;
+       /*  console.log(btn_index); */
 
-        e.target.classList.add('active'); // Add .active CSS Class
-        show_rating.innerHTML = e.target.textContent; // send the text content from the clicked button to the rating page
+        allBtn.forEach((btn, j) => {
+            if(btn_index >= j+1) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+            }
+
+        } )
+        show_rating.innerHTML = btn.innerHTML; // send the text content from the clicked button to the rating page
 
         submit.addEventListener('click' , () => {
             card.style.display = "none";
             rating.style.display = "block"; 
         });
-    }
-});
+    })    
+})
