8065c5c97786b6c5771a0563cc23089c71eddef1
Alina Apostu <alinagabriela.greu@gmail.com>
2022-11-23 21:34:12 +0100

    second commit

--- java/java.js
+++ java/java.js
@@ -1,34 +1,27 @@
-const submit = document.querySelector('#submit');
-
-const card = document.querySelector('#card');
-const rating = document.querySelector('#rating');
-
-let show_rating = document.querySelector('#show_rating');
-
-let allBtn = document.querySelectorAll('.btn_rating');
-/* console.log(allBtn); */
+const submit = document.querySelector("#submit");
+const card = document.querySelector("#card");
+const rating = document.querySelector("#rating");
+let show_rating = document.querySelector("#show_rating");
+let allBtn = document.querySelectorAll(".btn_rating");
 
 allBtn.forEach((btn, i) => {
-    btn.addEventListener('click', function() {
-        /* console.log(btn);
-        console.log(i); */
-        
-        let btn_index = i+1;
-       /*  console.log(btn_index); */
+  btn.addEventListener("click", function () {
 
-        allBtn.forEach((btn, j) => {
-            if(btn_index >= j+1) {
-                btn.classList.add('active');
-            } else {
-                btn.classList.remove('active');
-            }
+    let btn_index = i + 1;
+    /*  console.log(btn_index); */
 
-        } )
-        show_rating.innerHTML = btn.innerHTML; // send the text content from the clicked button to the rating page
+    allBtn.forEach((btn, j) => {
+      if (btn_index >= j + 1) {
+        btn.classList.add("active");
+      } else {
+        btn.classList.remove("active");
+      }
+    });
+    show_rating.innerHTML = btn.innerHTML; // send the text content from the clicked button to the rating page
 
-        submit.addEventListener('click' , () => {
-            card.style.display = "none";
-            rating.style.display = "block"; 
-        });
-    })    
-})
+    submit.addEventListener("click", () => {
+      card.style.display = "none";
+      rating.style.display = "block";
+    });
+  });
+});
